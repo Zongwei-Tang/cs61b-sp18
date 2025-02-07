3226f38e5f51fb49975531261081d2b173d61f1a
Zongwei Tang <tangzongwei070509@gmail.com>
2025-02-07 19:59:42 +0800

    Lab2 Complete

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,16 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null) {
+            return B;
+        }
+        IntList a=A;
+        while (A.rest!=null){
+            A=A.rest;
+        }
+        A.rest=B;
+        return a;
+
     }
 
     /**
@@ -91,7 +100,13 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A==null){
+            return B;
+        }
+        if (B==null){
+            return A;
+        }
+        return new IntList(A.first,catenate(A.rest,B));
     }
 
 
