a6b9854a31b2540c6314a38c472c327a1b95e946
Zongwei Tang <tangzongwei070509@gmail.com>
2025-04-06 21:50:36 +0800

    HW2 Improved 2

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -3,14 +3,29 @@ package hw2;
 import edu.princeton.cs.introcs.StdRandom;
 import edu.princeton.cs.introcs.StdStats;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class PercolationStats {
     private double[] value;
+
     public PercolationStats(int N, int T, PercolationFactory pf){
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("N 和 T 必须是正整数");
+        }
         value=new double [T];
         for (int i=0;i<T;i++) {
-            Percolation a=pf.make(N);
-            while (!a.percolates()) {
-                a.open(StdRandom.uniform(0, N), StdRandom.uniform(0, N));
+            Percolation a = pf.make(N);
+            List<Integer> b= new ArrayList<>();
+            for (int j=0;j<N*N;j++){
+                b.add(j);
+            }
+            Collections.shuffle(b);
+            int x=0;
+            while (!a.percolates() && x<b.size()) {
+                a.open((b.get(x))/N, (b.get(x))%N);
+                x++;
             }
             value[i]=(double)a.numberOfOpenSites()/(N*N);
         }
