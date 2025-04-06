8f2ee4fe3336dd8c0b98f9bace62654b6ed73c24
Zongwei Tang <tangzongwei070509@gmail.com>
2025-04-06 20:33:12 +0800

    HW2 completed

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,104 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    public class block {
+        boolean color;
 
+        public block(boolean a) {
+            color = a;
+        }
+    }
+
+    block[][] world;
+    int size;
+    WeightedQuickUnionUF uf;
+
+    public int index(int a, int b) {
+        return a * size + b;
+    }
+
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException();
+        }
+        world = new block[N][N];
+        size = N;
+        uf=new WeightedQuickUnionUF(N * N);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                world[i][j] = new block(false);
+            }
+        }
+    }
+
+    public void open(int row, int col) {
+        if (row > size - 1 || col > size - 1 || row < 0 || col < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (world[row][col].color != true) {
+            world[row][col].color = true;
+            int index = index(row, col);
+            if (row > 0 && isOpen(row - 1, col)) {
+                uf.union(index, index(row - 1, col));
+            }
+
+            // 检查并连接下方相邻位置
+            if (row < size - 1 && isOpen(row + 1, col)) {
+                uf.union(index, index(row + 1, col));
+            }
+
+            // 检查并连接左侧相邻位置
+            if (col > 0 && isOpen(row, col - 1)) {
+                uf.union(index, index(row, col - 1));
+            }
+
+            // 检查并连接右侧相邻位置
+            if (col < size - 1 && isOpen(row, col + 1)) {
+                uf.union(index, index(row, col + 1));
+            }
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+        if (row > size - 1 || col > size - 1 || row < 0 || col < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        return world[row][col].color;
+    }
+
+    public boolean isFull(int row, int col) {
+        for (int i = 0; i < size; i++) {
+            if (uf.connected(index(0,i),index(row,col))){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int numberOfOpenSites(){
+        int count=0;
+        for (int i=0;i<size;i++){
+            for (int j=0;j<size;j++){
+                if (world[i][j].color){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public boolean percolates(){
+        for (int i=0;i<size;i++){
+            for (int j=0;j<size;j++){
+                if (uf.connected(index(size-1,i),index(0,j))){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args){
+
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,30 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 
+public class PercolationStats {
+    double[] value;
+    public PercolationStats(int N, int T, PercolationFactory pf){
+        Percolation a=pf.make(N);
+        value=new double [T];
+        for (int i=0;i<T;i++) {
+            while (!a.percolates()) {
+                a.open(StdRandom.uniform(0, N), StdRandom.uniform(0, N));
+            }
+            value[i]=(double)a.numberOfOpenSites()/(N*N);
+        }
+    }
+    public double mean(){
+        return StdStats.mean(value);
+    }
+    public double stddev(){
+        return StdStats.stddev(value);
+    }
+    public double confidenceLow(){
+        return mean()-1.96*stddev()/Math.sqrt(value.length);
+    }
+    public double confidenceHigh(){
+        return mean()+1.96*stddev()/Math.sqrt(value.length);
+    }
 }
